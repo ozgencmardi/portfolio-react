87e10bd35216e8b47f625da4d35a5d9e6384b037
ozgencmardi <ozgencmardi@gmail.com>
2023-03-13 10:14:17 +0000

    Header added

---
+++ reactportfolio/src/components/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Navbar, Nav } from 'react-bootstrap';
+
+function Header() {
+  return (
+    <Navbar bg="light" expand="lg">
+      <Navbar.Brand href="#home">M. Ozgenc Mardi</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#services">Services</Nav.Link>
+          <Nav.Link href="#work">Work</Nav.Link>
+          <Nav.Link href="#portfolio">Portfolio</Nav.Link>
+          <Nav.Link href="#contact">Contact</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+}
+
+export default Header;
