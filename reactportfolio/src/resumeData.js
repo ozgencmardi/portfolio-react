f280f39395050158788598ae17d7636858aa8726
ozgencmardi <ozgencmardi@gmail.com>
2023-03-20 19:27:01 +0000

    resumeData.js created

---
+++ reactportfolio/src/resumeData.js
@@ -0,0 +1,93 @@
+let resumeData = {
+    "imagebaseurl":"https://github.com/ozgencmardi",
+    "name": "Metin Ozgenc Mardi",
+    "role": "Web Developer/Designer",
+    "linkedinId":"ozgencmardi",
+    "roleDescription": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore, sequi quos! Reprehenderit at distinctio porro quam veniam provident recusandae ipsam consectetur nobis perspiciatis!",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/ozgencmardi",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/ozgencmardi",
+          "className":"fa fa-github"
+        }
+      ],
+    "aboutme":"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore, sequi quos! Reprehenderit at distinctio porro quam veniam provident recusandae ipsam consectetur nobis perspiciatis!",
+    "address":"London",
+    "website":"https://www.linkedin.com/in/ozgencmardi",
+    "education":[
+      {
+        "UniversityName":"Anadolu University",
+        "specialization":"Business Administration",
+        "MonthOfPassing":"June",
+        "YearOfPassing":"2012",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Wedelo Limited",
+        "specialization":"Web Developer/Designer",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2023",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "CompanyName":"Internet Express Limited",
+        "specialization":"Web Developer/Designer",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2020",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"Furniture Store",
+        "description":"HTML, CSS",
+        "imgurl":"img/furniture-store.png"
+      },
+      {
+        "name":"Semantic HTML",
+        "description":"HTML, CSS",
+        "imgurl":"img/semantic-html.png"
+      },
+      {
+        "name":"Travel California",
+        "description":"HTML, CSS",  
+        "imgurl":"img/travel-california.png"
+      },
+      {
+        "name":"CSS Snippet",
+        "description":"HTML, CSS",
+        "imgurl":"css-snippet.png"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
